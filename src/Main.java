20358f61bff428b4b0880f1fc1e067e18ff04aba
Aditi <donaaditi@gmail.com>
2022-08-18 14:07:31 +0530

    Min max values of float and double

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,20 @@ public class Main {
         int intVar = 4;
         long longVar = 50000 + 10*(byteVar + shortVar + intVar);
         System.out.println(longVar);
+
+        float maxValueFloat = Float.MAX_VALUE;
+        float minValueFloat = Float.MIN_VALUE;
+        System.out.println("Max value of Float is: " + maxValueFloat);
+        System.out.println("Min value of Float is: " + minValueFloat);
+
+        double maxValueDouble = Double.MAX_VALUE;
+        double minValueDouble = Double.MIN_VALUE;
+        System.out.println("Max value of Double is: " + maxValueDouble);
+        System.out.println("Min value of Double is: " + minValueDouble);
+
+        double pounds = 3.6;
+        double poundsInKgs = pounds * 0.45359237;
+        System.out.println(poundsInKgs);
+        
     }
 }
